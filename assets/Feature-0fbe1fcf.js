054858a31b6c2bfddcba444f7369427087daf414
Bekjon Ibragimov <bekjon_98@mail.ru>
2022-12-20 18:17:51 +0200

    dist added

--- assets/Feature-0fbe1fcf.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as s,a as t}from"./index-1a82d278.js";const c=({heading:a,content:r})=>e(s,{children:t("div",{className:"feature",children:[e("div",{className:"feature__heading",children:a}),e("div",{className:"feature__content",children:r})]})});export{c as F};
---
+++ assets/Feature-11e8e041.js
@@ -0,0 +1,1 @@
+import{j as e,F as s,a as t}from"./index-7b3fd7ba.js";const c=({heading:a,content:r})=>e(s,{children:t("div",{className:"feature",children:[e("div",{className:"feature__heading",children:a}),e("div",{className:"feature__content",children:r})]})});export{c as F};
